41d124a5b7f1c47b4000ac4c7697d876cfa0f697
Yen <yychen@mirrormedia.mg>
2020-04-17 02:43:57 +0800

    1. Trim config entries 2. Drone: remove prod branch behavior for future implementation 3. Drone: add auto-deploy for dev branch

--- config/config.go
+++ config/config.go
@@ -22,99 +22,12 @@ type AppConfig struct {
 		TrasactionIDPlaceholder string                       `mapstructure:"trasaction_id_placeholder"`
 	} `mapstructure:"sql"`
 
-	Redis struct {
-		ReadURL  string `mapstructure:"read_url"`
-		WriteURL string `mapstructure:"write_url"`
-		Password string `mapstructure:"password"`
-		Cache    struct {
-			LatestCommentCount int `mapstructure:"latest_comment_count"`
-			NotificationCount  int `mapstructure:"notification_count"`
-		} `mapstructure:"cache"`
-	} `mapstructure:"redis"`
-
-	ES struct {
-		Url        string `mapstructure:"url"`
-		LogIndices string `mapstructure:"log_indices"`
-	} `mapstructure:"es"`
-
-	Crawler struct {
-		Headers map[string]string `mapstructure:"headers"`
-	} `mapstructure:"crawler"`
-
-	Mail struct {
-		Host         string `mapstructure:"host"`
-		Port         int    `mapstructure:"port"`
-		User         string `mapstructure:"user"`
-		Password     string `mapstructure:"password"`
-		UserName     string `mapstructure:"user_name"`
-		DevTeam      string `mapstructure:"dev_team"`
-		Enable       bool   `mapstructure:"enable"`
-		TemplatePath string `mapstructure:"template_path"`
-	} `mapstructure:"mail"`
-
-	SearchFeed struct {
-		Host      string `mapstructure:"host"`
-		IndexName string `mapstructure:"index_name"`
-		MaxRetry  int    `mapstructure:"max_retry"`
-	} `mapstructure:"search_feed"`
-
 	Models struct {
-		Assets                map[string]int `mapstructure:"assets"`
-		AssetType             map[string]int `mapstructure:"asset_type"`
-		AssetCopyright        map[string]int `mapstructure:"asset_copyright"`
-		Cards                 map[string]int `mapstructure:"cards"`
-		CardStatus            map[string]int `mapstructure:"card_status"`
-		Members               map[string]int `mapstructure:"members"`
-		MemberDailyPush       map[string]int `mapstructure:"member_daily_push"`
-		MemberPostPush        map[string]int `mapstructure:"member_post_push"`
-		Posts                 map[string]int `mapstructure:"posts"`
-		PostType              map[string]int `mapstructure:"post_type"`
-		PostPublishStatus     map[string]int `mapstructure:"post_publish_status"`
-		Tags                  map[string]int `mapstructure:"tags"`
-		TaggingType           map[string]int `mapstructure:"tagging_type"`
-		ProjectsActive        map[string]int `mapstructure:"projects_active"`
-		ProjectsStatus        map[string]int `mapstructure:"projects_status"`
-		ProjectsPublishStatus map[string]int `mapstructure:"projects_publish_status"`
-		Memos                 map[string]int `mapstructure:"memos"`
-		MemosPublishStatus    map[string]int `mapstructure:"memos_publish_status"`
-		Comment               map[string]int `mapstructure:"comment"`
-		CommentStatus         map[string]int `mapstructure:"comment_status"`
-		ReportedCommentStatus map[string]int `mapstructure:"reported_comment_status"`
-		Reports               map[string]int `mapstructure:"reports"`
-		ReportsPublishStatus  map[string]int `mapstructure:"reports_publish_status"`
-		FollowingType         map[string]int `mapstructure:"following_type"`
-		Emotions              map[string]int `mapstructure:"emotions"`
-		PointType             map[string]int `mapstructure:"point_type"`
-		PointStatus           map[string]int `mapstructure:"point_status"`
-		HotTagsWeight         map[string]int `mapstructure:"hot_tags_wieght"`
-		Promotions            map[string]int `mapstructure:"promotions"`
+		Members       map[string]int `mapstructure:"members"`
+		FollowingType map[string]int `mapstructure:"following_type"`
 	} `mapstructure:"models"`
 
-	ReadrID      int    `mapstructure:"readr_id"`
-	DefaultOrder int    `mapstructure:"default_order"`
-	DomainName   string `mapstructure:"domain_name"`
-	TokenSecret  string `mapstructure:"token_secret"`
-
-	PaymentService struct {
-		PartnerKey         string `mapstructure:"partner_key"`
-		MerchantID         string `mapstructure:"merchant_id"`
-		PrimeURL           string `mapstructure:"prime_url"`
-		TokenURL           string `mapstructure:"token_url"`
-		Currency           string `mapstructure:"currency"`
-		PaymentDescription string `mapstructure:"payment_description"`
-	} `mapstructure:"payment_service"`
-
-	InvoiceService struct {
-		MerchantID string `mapstructure:"merchant_id"`
-		URL        string `mapstructure:"url"`
-		APIVersion string `mapstructure:"api_version"`
-		Key        string `mapstructure:"key"`
-		IV         string `mapstructure:"iv"`
-	} `mapstructure:"invoice_service"`
-
-	Slack struct {
-		NotifyWebhook string `mapstructure:"notify_webhook"`
-	} `mapstructure:"slack"`
+	DomainName string `mapstructure:"domain_name"`
 }
 
 func LoadConfig(configPath string) (conf AppConfig, err error) {
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/golang-migrate/migrate v3.5.4+incompatible
 	github.com/jmoiron/sqlx v1.2.0
+	github.com/prometheus/client_golang v0.9.3
 	github.com/readr-media/readr-restful v0.0.0-20200227100724-794e60171429
 	github.com/spf13/viper v1.6.2
 	github.com/stretchr/testify v1.4.0
